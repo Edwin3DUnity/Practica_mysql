c3907d365bd06b2728aa1c2a9810f068072d19e4
Edwin unity <eapaniagua8@misena.edu.co>
2024-10-14 10:31:51 -0500

    Login mostrados en unity , de la BD y la www

--- Assets/Scripts/GestionBD.cs
+++ Assets/Scripts/GestionBD.cs
@@ -7,7 +7,8 @@ public class GestionBD : MonoBehaviour
 {
     public InputField txtUsuario;
     public InputField txtContraseña;
-    
+    public string     nombreUsuario;
+    public int        scoreUsuario;
     
     
     
@@ -16,6 +17,7 @@ public class GestionBD : MonoBehaviour
     public void iniciarSesion()
     {
         StartCoroutine(Login());
+        StartCoroutine(Datos());
     }
     
     // Start is called before the first frame update
@@ -47,7 +49,30 @@ public class GestionBD : MonoBehaviour
          print("Error en la coneccion");
      }
     }
-    
+    IEnumerator Datos()
+    {
+        WWW coneccion = new WWW("http://localhost/pract/datos.php?uss="+ txtUsuario.text );
+        yield return(coneccion);
+        //   Debug.Log(coneccion.text);
+         if (coneccion.text == "401")
+        {
+            print("Usuario incorrectos");
+        }
+         else
+         {
+             string[] nDdatos = coneccion.text.Split('|');
+             if (nDdatos.Length != 3)
+             {
+                 print("Error en la coneccion ndatos");
+             }
+             else
+             {
+                 nombreUsuario = nDdatos[0];
+                 scoreUsuario = int.Parse(nDdatos[1]);
+             }
+         }
+      
+    }
     
     
 }
